9b3def0d32b4e500841f13fb3954d697486ed669
Kosuke Takeu <n.kosuke0906@gmail.come>
2023-07-17 12:41:33 +0900

    supabaseにuser情報を登録する

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth, { AuthOptions } from 'next-auth';
 import LineProvider from 'next-auth/providers/line';
 import { SupabaseAdapter } from '@next-auth/supabase-adapter';
 
-const options: AuthOptions = {
+export const options: AuthOptions = {
   providers: [
     LineProvider({
       clientId: process.env.LINE_CLIENT_ID ?? '',
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image';
-import { LoginButton } from './components/LoginButton';
 import { getServerSession } from 'next-auth';
+import { options } from './api/auth/[...nextauth]/route';
+import { LoginButton } from './components/LoginButton';
 import { SignOutButton } from './components/SignOutButton';
 
 export default async function Home() {
-  const session = await getServerSession();
-
-  console.log(session);
+  const session = await getServerSession(options);
+  const user = session?.user;
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-        {session ? (
+        {user ? (
           <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
             <a
               className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
@@ -31,9 +31,9 @@ export default async function Home() {
         ) : (
           <LoginButton />
         )}
-        {session ? (
+        {user ? (
           <div className='flex gap-4'>
-            <p>{session.user?.name}</p>
+            <p>{user?.name}</p>
             <SignOutButton />
           </div>
         ) : (
